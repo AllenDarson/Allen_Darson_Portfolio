fd6530591e494abd961d0747425384a75d829459
Allen Darson <allendarson27@gmail.com>
2025-08-21 16:59:44 +0530

    Updated portfolio with latest changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -29,9 +29,20 @@ const Home = () => {
           <a className="navbar-brand fw-bold fs-4" href="#home">
             <PiHandPeaceDuotone /> Allen Darson C
           </a>
-          <Button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+
+          {/* FIXED TOGGLER BUTTON */}
+          <button
+            className="navbar-toggler ms-auto"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
-          </Button>
+          </button>
+
           <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item mx-2"><a className="nav-link" href="#home"><AiOutlineHome /> Home</a></li>
