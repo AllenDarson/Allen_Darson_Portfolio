79b17b1a010636119724c59fb490ccd8468fb12e
Allen Darson <allendarson27@gmail.com>
2025-11-30 21:40:36 +0530

    Spelling corrections on my old pdf

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import resumeFile from '../Images/myResume.pdf';
+import resumeFile from '../Images/Allen_Darson_Resume_Wipro.pdf';
 
 import { motion } from 'framer-motion';
 import { Button } from 'react-bootstrap';
@@ -101,7 +101,7 @@ const Home = () => {
 
               <motion.a
                 href={resumeFile}
-                download="myResume"
+                download="Allen_Darson_Resume_Wipro.pdf"
                 whileHover={{ scale: 1.05 }}
                 className="btn btn-dark me-3"
               >
